a8a204ced524ab92c9142a85cfb8702be0f73efb
Clemens Lange <clemens.lange@cern.ch>
2020-05-04 17:19:16 +0200

    cd to cmsusr home directory before setting up CMSSW release

--- standalone/entrypoint.sh
+++ standalone/entrypoint.sh
@@ -3,6 +3,7 @@ set  -e
 
 echo "Setting up ${CMSSW_VERSION}"
 source /opt/cms/cmsset_default.sh
+cd /home/cmsusr
 scramv1 project CMSSW ${CMSSW_VERSION}
 cd ${CMSSW_VERSION}/src
 eval `scramv1 runtime -sh`
